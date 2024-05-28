bce90da82378e63460e70f3f7fdc51dddb5cf0db
NeoCrao <neocrao@gmail.com>
2024-05-27 19:24:25 -0700

    Try with copy-webpack-plugin

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,4 +1,5 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+const CopyPlugin = require("copy-webpack-plugin");
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
@@ -6,5 +7,13 @@ const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
+  }),
+  new CopyPlugin({
+    patterns: [
+      {
+        from: "./node_modules/sharp/",
+        to: "./output/node_modules/sharp/", // still under node_modules directory so it could find this module
+      },
+    ],
   }),
 ];
